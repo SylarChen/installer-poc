706a4823314576fea48c9e80de080101a6ba1108
Sylar Chen <chenheng415@163.com>
2016-08-25 21:28:31 +0800

    Add post method handle create pod request

--- src/main/java/suiteinstaller/SuiteInstallerController.java
+++ src/main/java/suiteinstaller/SuiteInstallerController.java
@@ -1,8 +1,10 @@
 package suiteinstaller;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/suiteinstaller")
 public class SuiteInstallerController {
-
+	
 	@RequestMapping(value="/createPod", method=RequestMethod.GET)
 	@ResponseBody
     public String createPod() {
@@ -24,16 +26,57 @@ public class SuiteInstallerController {
         return "Pod is createing";
     }
 	
-	//https://spring.io/guides/tutorials/bookmarks/
-//	@RequestMapping(value="/installsuite", method=RequestMethod.POST)
-//	@ResponseBody
-//    public String greeting(@RequestBody requestBody) {
-//        return "greeting";
-//    }
+	@RequestMapping(value="/install", method=RequestMethod.POST)
+	@ResponseBody
+    public String greeting(@RequestBody Suite suite) {
+		System.out.println(suite);
+		String yamlFolder = "/pv_suite_install/" + suite.getSuiteName() + "/output/";
+		System.out.println("yamlFolder : " + yamlFolder);
+		
+		try {
+			for(String yaml : suite.getYamlList()){
+				String path = yamlFolder + yaml;
+				System.out.println("Create pod : " + path);
+				String[] commands = {"/createsinglepod.sh", path};
+				Runtime.getRuntime().exec(commands);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return "Creating yaml : " + suite.getYamlList();
+	}
+}
+
+/*
+{
+    "suiteName" : "itsma",
+    "yamlList" : ["test1.yaml, test2.yaml"]
+}
+*/
+
+class Suite {
+	@Override
+	public String toString() {
+		return "Suite [suiteName=" + suiteName + ", yamlList=" + yamlList + "]";
+	}
+
+	private String suiteName;
+    private List<String> yamlList;
 	
-//	@RequestMapping(value="/installsuite", method=RequestMethod.POST)
-//	@ResponseBody
-//    public String greeting(@RequestParam(value="suite", required=true) String suite) {
-//        return "greeting";
-//    }
+    public String getSuiteName() {
+		return suiteName;
+	}
+
+	public List<String> getYamlList() {
+		return yamlList;
+	}
+
+    public void setSuiteName(String suiteName) {
+        this.suiteName = suiteName;
+    }
+    
+    public void setYamlList(List<String> yamlList) {
+        this.yamlList = yamlList;
+    }
 }
