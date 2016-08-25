925cec531c98ae4c7b246002162c70c6d4d5bae8
Sylar Chen <chenheng415@163.com>
2016-08-25 21:30:57 +0800

    update Docker K8S related files

--- Docker/deleteAll.sh
+++ Docker/deleteAll.sh
@@ -1,4 +1,8 @@
 kubectl --namespace=kube-system delete -f suiteconfig_svc.yaml
 kubectl --namespace=kube-system delete -f suiteconfig.yaml
 kubectl --namespace=kube-system delete -f suiteinstaller.yaml
+kubectl --namespace=kube-system delete -f suite_yamls/test1.yaml
+kubectl --namespace=kube-system delete -f suite_yamls/test2.yaml
+
+rm -rf /var/vols/ITOM/suite-install
 
