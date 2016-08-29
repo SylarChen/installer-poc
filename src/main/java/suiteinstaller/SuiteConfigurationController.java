4fdb08f26a16b787e55e0ee8ebe7bca23cb24da1
Chen, Heng (Sylar,XS-SH) heng.chen@hp.com <heng.chen@hp.com>
2016-08-29 13:54:05 +0800

    Add logic edit configmap

--- src/main/java/suiteinstaller/SuiteConfigurationController.java
+++ src/main/java/suiteinstaller/SuiteConfigurationController.java
@@ -1,6 +1,9 @@
 package suiteinstaller;
 
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,16 +11,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.esotericsoftware.yamlbeans.YamlReader;
+import com.esotericsoftware.yamlbeans.YamlWriter;
+
 @Controller
 @RequestMapping("/suiteconf")
 public class SuiteConfigurationController {
 	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@RequestMapping(value="/copy", method=RequestMethod.GET)
 	@ResponseBody
     public String copy(@RequestParam(value="user", required=false, defaultValue="postgres") String user, 
     		@RequestParam(value="password", required=false, defaultValue="postgres") String password) {
 		System.out.println("Going to edit config map, user input user/pwd: " + user + "/" + password);
+
 		try {
+			//replace var in configmap
+//			String yamlFile = "C:\\WorkSpace\\MyGitHub\\installer-poc\\src\\main\\resources\\configmap.yaml";
+			String yamlFile = "/suite_yamls/configmap.yaml";
+			YamlReader reader = new YamlReader(new FileReader(yamlFile));
+			Map configmap = (Map) reader.read();
+			Map data = (Map) configmap.get("data");
+			System.out.println("default pg.user: " + data.get("pg.user"));
+			System.out.println("default pg.password: " + data.get("pg.pwd"));
+			data.put("pg.user", user);
+			data.put("pg.pwd", password);
+			YamlWriter writer = new YamlWriter(new FileWriter(yamlFile));
+			writer.write(configmap);
+			writer.close();
+			//----------------------------------------------------------------------------------------------
+			
+			//copy yamls to PV
 			Runtime.getRuntime().exec("/copyYamls.sh");
 		} catch (IOException e) {
 			e.printStackTrace();
