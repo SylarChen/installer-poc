4b852aa48b1664a36bdf177b2eec3d518d6fc439
Chen, Heng (Sylar,XS-SH) heng.chen@hp.com <heng.chen@hp.com>
2016-08-29 11:28:55 +0800

    receive frontend configmap input

--- src/main/java/suiteinstaller/SuiteConfigurationController.java
+++ src/main/java/suiteinstaller/SuiteConfigurationController.java
@@ -5,15 +5,18 @@ import java.io.IOException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/suiteconf")
 public class SuiteConfigurationController {
-
+	
 	@RequestMapping(value="/copy", method=RequestMethod.GET)
 	@ResponseBody
-    public String copy() {
+    public String copy(@RequestParam(value="user", required=false, defaultValue="postgres") String user, 
+    		@RequestParam(value="password", required=false, defaultValue="postgres") String password) {
+		System.out.println("Going to edit config map, user input user/pwd: " + user + "/" + password);
 		try {
 			Runtime.getRuntime().exec("/copyYamls.sh");
 		} catch (IOException e) {
@@ -21,6 +24,7 @@ public class SuiteConfigurationController {
 		}
         return "Copy yamls to /var/vols/ITOM/suite-install/itsma/output";
     }
+	
 	
 	
 	@RequestMapping(value="/finish", method=RequestMethod.GET)
